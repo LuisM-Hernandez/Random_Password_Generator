4d07e70fc5090a8d6cdecaed1c7830dd15e4576c
Luis <m.luishernandez@outlook.com>
2021-06-12 18:47:03 -0400

    Added gePassword function

--- script.js
+++ script.js
@@ -92,16 +92,59 @@ var upperCasedCharacters = [
 
 
 // Assignment Code
+//Select the button by using #generate selector
 var generateBtn = document.querySelector("#generate");
+
+function getPasswordOptions() {
+  //Variable that stores the lenght of the password by users options
+  var length = parseInt(prompt("How many characters would you like your password to contain?"));
+
+  //The global NaN property is a value representing Not-A-Number.
+   if (isNaN(length) === true) {
+    alert('Password length must be provided as a number');
+    return;
+  }
+
+  if (length < 8) {
+    alert("Password lenght must be at least 8 characters");
+    return;
+  }
+
+  if (length > 128) {
+    alert("Password lenght must be less than 129 characters");
+    
+  }
+
+  var addSpecialCharacters = confirm("Would you like to include special characters?");
+  var addNumericCharacters = confirm("Would you like to include numerics characters?");
+  var addLowerCharacters = confirm("Would you like to include lowercase characters?");
+  var addUpperCharacters = confirm("Would you like to include upper characters?");
+
+}
+
+
+function generatePassword() {
+  //To generate password I need to abide by this rules:
+  //1. Prompt the user to a lenght of 8 characters minimum or 128 max
+  //2.Prompt the user to password criteria (Special Char, Num, low, etc)
+
+  var options = getPasswordOptions();
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
+  //Variable password holding the function generatePassword
   var password = generatePassword();
+  // Variable passwordText holding selector #password
   var passwordText = document.querySelector("#password");
-
+  //passwordText value is equal to password
   passwordText.value = password;
-
 }
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
