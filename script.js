354ecb29bb9c93017ad7b5ccd72254880286b0bb
Luis <m.luishernandez@outlook.com>
2021-06-14 15:02:55 -0400

    Added user choices and var holding the user choices

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Array of special characters to be included in password
-var specialCharacters = [
+var special = [
   '@',
   '%',
   '+',
@@ -26,11 +26,11 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+var lower = [
   'a',
   'b',
   'c',
@@ -60,7 +60,7 @@ var lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+var upper = [
   'A',
   'B',
   'C',
@@ -92,6 +92,13 @@ var upperCasedCharacters = [
 
 
 // Assignment Code
+
+//Variable will hold user choice
+var userChoice;
+
+//
+var userPass = []
+
 //Select the button by using #generate selector
 var generateBtn = document.querySelector("#generate");
 
@@ -116,30 +123,89 @@ function getPasswordOptions() {
     
   }
 
-  var addSpecialCharacters = confirm("Would you like to include special characters?");
-  var addNumericCharacters = confirm("Would you like to include numerics characters?");
-  var addLowerCharacters = confirm("Would you like to include lowercase characters?");
-  var addUpperCharacters = confirm("Would you like to include upper characters?");
+  else{
+    var addSpecialCharacters = confirm("Would you like to include special characters?");
+    var addNumericCharacters = confirm("Would you like to include numerics characters?");
+    var addLowerCharacters = confirm("Would you like to include lowercase characters?");
+    var addUpperCharacters = confirm("Would you like to include upper characters?");
+    
+  }
+
 
   if (addSpecialCharacters === false && addNumericCharacters === false && addLowerCharacters === false && addUpperCharacters === false) {
     alert("Must contain at least one option");
     return;
   }
-  
+
+  //Special Characters choices
+  if (addSpecialCharacters === true) {
+    userChoice = special
+        
+  }
+  if (addSpecialCharacters === true && addNumericCharacters === true) {
+    userChoice = special.concat(numeric)
+        
+  }
+
+  if (addSpecialCharacters === true && addNumericCharacters === true && addLowerCharacters === true) {
+    userChoice = special.concat(numeric, lower);
+        
+  }
+
+  if (addSpecialCharacters === true && addNumericCharacters === true && addLowerCharacters === true && addUpperCharacters === true) {
+    userChoice = special.concat(numeric, lower, upper);
+  }
+
+  //Numeric Characters choices
+  if (addNumericCharacters === true) {
+    userChoice = numeric
+        
+  }
+  if (addNumericCharacters === true && addLowerCharacters === true) {
+    userChoice = numeric.concat(lower)
+        
+  }
+
+  if (addNumericCharacters === true && addLowerCharacters === true && addUpperCharacters === true) {
+    userChoice = numeric.concat(lower, upper);
+        
+  }
+
+  //Lower Characters choices
+  if (addLowerCharacters === true) {
+    userChoice = lower
+        
+  }
+  if (addLowerCharacters === true && addUpperCharacters === true) {
+    userChoice = lower.concat(upper)
+        
+  }
+
+  if (addLowerCharacters === true && addSpecialCharacters === true) {
+    userChoice = lower.concat(special);
+        
+  }
+
+  //Upper Characters choices
+  if (addUpperCharacters === true) {
+    userChoice = upper
+        
+  }
+  if (addUpperCharacters === true && addSpecialCharacters === true) {
+    userChoice = upper.concat(special)
+        
+  }
+
 
 }
 
 
 function generatePassword() {
-  //To generate password I need to abide by this rules:
-  //1. Prompt the user to a lenght of 8 characters minimum or 128 max
-  //2.Prompt the user to password criteria (Special Char, Num, low, etc)
 
-  var options = getPasswordOptions();
+  getPasswordOptions();
+  
+
 }
-
-
-
 // Write password to the #password input
 function writePassword() {
   //Variable password holding the function generatePassword
@@ -149,9 +215,6 @@ function writePassword() {
   //passwordText value is equal to password
   passwordText.value = password;
 }
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
